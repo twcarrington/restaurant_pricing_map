365b5bd407b24c1e44fcf506b21535424f9444f7
Tommy <twcarrington@gmail.com>
2017-05-18 13:48:26 -0400

    changing file names worked.  For some reason, following the documentation's example caused test runner to ignore tests on react components.  Naming them like any other JS component did the trick

--- src/Link.test.js
+++ src/Link.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from './Link.react';
+import Link from './Link.js';
 import renderer from 'react-test-renderer';
 
 test('Link changes the class when hovered', () => {
