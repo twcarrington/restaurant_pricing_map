216f42c58f4a545bc41bb4d9cf66eadce8c6b9b3
Tommy <twcarrington@gmail.com>
2017-06-30 21:41:13 -0400

    firebase.json public property now set up to work with firebase CLI package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAmv3U2SEJNl9qWpP1H7-E98bTMWosY9bk",
+  authDomain: "restaurant-pricing-map.firebaseapp.com",
+  databaseURL: "https://restaurant-pricing-map.firebaseio.com",
+  projectId: "restaurant-pricing-map",
+  storageBucket: "restaurant-pricing-map.appspot.com",
+  messagingSenderId: "290077590095"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
